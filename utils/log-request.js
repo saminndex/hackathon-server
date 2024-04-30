70e00ae36b1b360385519c1fb6b24da124d5555a
Sam Addadahine <sam.adda2@gmail.com>
2024-04-30 14:36:00 +0100

    improved logging

--- utils/log-request.js
+++ utils/log-request.js
@@ -1,6 +1,6 @@
 const logRequestDetails = (req, res, next) => {
   try {
-    console.info(`${req.ip}: ${req.originalUrl}, body: ${req.body}`);
+    console.info(`${req.ip}: ${req.originalUrl}, body: ${JSON.stringify(req.body)}`);
   } catch (error) {
     console.error("Error logging request details:", error);
   }
